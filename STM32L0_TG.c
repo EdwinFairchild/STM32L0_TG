f4631a507691af8e433f20ab64bf933919fce0f6
eddie <edwin@edwinfairchild.com>
2020-08-05 00:49:36 -0400

    minor

--- STM32L0_TG.c
+++ STM32L0_TG.c
@@ -4,7 +4,7 @@
 #include <stm32l0xx_ll_spi.h>
 #include <stm32l0xx_ll_usart.h>
 #include <stm32l0xx_ll_adc.h>
-
+#include <stm32l0xx_ll_dma.h>
 //---------------| CL |------------------------
 #include  "CL_CONFIG.h"
 #include "CL_printMsg.h"
@@ -17,6 +17,8 @@
 #include "testimg.h"
 #include "testimg2.h"
 
+//---------------| GLOBALS |-------------------
+uint32_t posVal[2] ,xVal_pre, yVal_pre = 0;
 //---------------| Prototypes |----------------
 void init_CL(void);
 void initLed(void);
@@ -82,6 +84,9 @@ void crayLoop(void)
 }
 void MX_ADC_Init(void);
 long map(long x, long in_min, long in_max, long out_min, long out_max);
+void MX_DMA_Init(void);
+
+
 int main(void)
 {
 	init_CL();
@@ -106,8 +111,15 @@ int main(void)
 	{
 		//ST7735_FillScreen(ST7735_BLACK);
 		LL_ADC_REG_StartConversion(ADC1);
+		if (LL_ADC_IsActiveFlag_EOC(ADC1))
+		{
+			num = map(LL_ADC_REG_ReadConversionData12(ADC1), 0, 4095, 7, 153);
+			LL_ADC_ClearFlag_EOC(ADC1);
+			LL_ADC_ClearFlag_EOS(ADC1);
+		}
 		delayMS(10);
-		num = map(LL_ADC_REG_ReadConversionData12(ADC1), 0, 4095, 0, 160);
+		
+		
 		ST7735_printMsg(10, 10, " %d  ", num);
 		if (num != numPrev) //if at a new spot
 		{
@@ -233,16 +245,50 @@ void MX_ADC_Init(void)
   
 	LL_IOP_GRP1_EnableClock(LL_IOP_GRP1_PERIPH_GPIOB);
 	/**ADC GPIO Configuration  
+	PB0   ------> ADC_IN8
 	PB1   ------> ADC_IN9 
 	*/
+	GPIO_InitStruct.Pin = LL_GPIO_PIN_0;
+	GPIO_InitStruct.Mode = LL_GPIO_MODE_ANALOG;
+	GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
+	LL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
 	GPIO_InitStruct.Pin = LL_GPIO_PIN_1;
 	GPIO_InitStruct.Mode = LL_GPIO_MODE_ANALOG;
 	GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
 	LL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
+	/* ADC DMA Init */
+  
+	/* ADC Init */
+	LL_DMA_SetPeriphRequest(DMA1, LL_DMA_CHANNEL_1, LL_DMA_REQUEST_0);
+
+	LL_DMA_SetDataTransferDirection(DMA1, LL_DMA_CHANNEL_1, LL_DMA_DIRECTION_PERIPH_TO_MEMORY);
+
+	LL_DMA_SetChannelPriorityLevel(DMA1, LL_DMA_CHANNEL_1, LL_DMA_PRIORITY_LOW);
+
+	LL_DMA_SetMode(DMA1, LL_DMA_CHANNEL_1, LL_DMA_MODE_NORMAL);
+
+	LL_DMA_SetPeriphIncMode(DMA1, LL_DMA_CHANNEL_1, LL_DMA_PERIPH_NOINCREMENT);
+
+	LL_DMA_SetMemoryIncMode(DMA1, LL_DMA_CHANNEL_1, LL_DMA_MEMORY_INCREMENT);
+
+	LL_DMA_SetPeriphSize(DMA1, LL_DMA_CHANNEL_1, LL_DMA_PDATAALIGN_HALFWORD);
+
+	LL_DMA_SetMemorySize(DMA1, LL_DMA_CHANNEL_1, LL_DMA_MDATAALIGN_HALFWORD);
+	
+	LL_DMA_SetDataLength(DMA1, LL_DMA_CHANNEL_1, 2);
+	
+	LL_DMA_SetMemoryAddress(DMA1, LL_DMA_CHANNEL_1 , &posVal);
+	
+	LL_DMA_SetPeriphAddress(DMA1, LL_DMA_CHANNEL_1, &ADC1->DR);
+
 	/* USER CODE BEGIN ADC_Init 1 */
 
 	/* USER CODE END ADC_Init 1 */
+	/** Configure Regular Channel 
+	*/
+	LL_ADC_REG_SetSequencerChAdd(ADC1, LL_ADC_CHANNEL_8);
 	/** Configure Regular Channel 
 	*/
 	LL_ADC_REG_SetSequencerChAdd(ADC1, LL_ADC_CHANNEL_9);
@@ -250,9 +296,9 @@ void MX_ADC_Init(void)
 	*/
 	ADC_REG_InitStruct.TriggerSource = LL_ADC_REG_TRIG_SOFTWARE;
 	ADC_REG_InitStruct.SequencerDiscont = LL_ADC_REG_SEQ_DISCONT_DISABLE;
-	ADC_REG_InitStruct.ContinuousMode = LL_ADC_REG_CONV_SINGLE;
-	ADC_REG_InitStruct.DMATransfer = LL_ADC_REG_DMA_TRANSFER_NONE;
-	ADC_REG_InitStruct.Overrun = LL_ADC_REG_OVR_DATA_PRESERVED;
+	ADC_REG_InitStruct.ContinuousMode = LL_ADC_REG_CONV_CONTINUOUS;
+	ADC_REG_InitStruct.DMATransfer = LL_ADC_REG_DMA_TRANSFER_UNLIMITED;
+	ADC_REG_InitStruct.Overrun = LL_ADC_REG_OVR_DATA_OVERWRITTEN;
 	LL_ADC_REG_Init(ADC1, &ADC_REG_InitStruct);
 	LL_ADC_SetSamplingTimeCommonChannels(ADC1, LL_ADC_SAMPLINGTIME_1CYCLE_5);
 	LL_ADC_SetOverSamplingScope(ADC1, LL_ADC_OVS_DISABLE);
@@ -266,9 +312,24 @@ void MX_ADC_Init(void)
 	ADC_InitStruct.DataAlignment = LL_ADC_DATA_ALIGN_RIGHT;
 	ADC_InitStruct.LowPowerMode = LL_ADC_LP_MODE_NONE;
 	LL_ADC_Init(ADC1, &ADC_InitStruct);
-	LL_ADC_Enable(ADC1);
 	/* USER CODE BEGIN ADC_Init 2 */
 
 	/* USER CODE END ADC_Init 2 */
 
+
 }
+
+void MX_DMA_Init(void) 
+{
+
+	/* Init with LL driver */
+	/* DMA controller clock enable */
+	LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_DMA1);
+
+	/* DMA interrupt init */
+	/* DMA1_Channel1_IRQn interrupt configuration */
+	NVIC_SetPriority(DMA1_Channel1_IRQn, 0);
+	NVIC_EnableIRQ(DMA1_Channel1_IRQn);
+
+}
+
